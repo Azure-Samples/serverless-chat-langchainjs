7119f81202136092fd17f405f65af6e47ed62cdd
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-02-15 18:42:42 +0000

    chore: Remove get-hello-test function

--- packages/api/src/functions/get-hello-test.ts
+++
@@ -1,9 +0,0 @@
-import { type HttpRequest, type HttpResponseInit, type InvocationContext } from '@azure/functions';
-
-export async function GetHelloTest(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-  context.log(`Http function processed request for url "${request.url}"`);
-
-  const phrase = request.query.get('name') || (await request.text()) || 'API Chat!';
-
-  return { body: `Hello, ${phrase}!` };
-}
