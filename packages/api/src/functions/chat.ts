7b31fdb7217fbd3f4f9aaff8e80c2e45e4161d28
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-03-09 02:52:53 +0000

    refactor: `chat.ts` file and add HTTP response utility functions

--- packages/api/src/functions/chat.ts
+++ packages/api/src/functions/chat.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
 import { AzureOpenAIEmbeddings } from '@langchain/azure-openai';
 import 'dotenv/config';
+import { badRequest, serviceUnavailable, ok, internalServerError } from '../utils';
 
 export async function chat(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
@@ -9,12 +10,7 @@ export async function chat(request: HttpRequest, context: InvocationContext): Pr
     const requestBody: any = await request.json();
 
     if (!requestBody?.question) {
-      return {
-        status: 400,
-        jsonBody: {
-          error: 'No question provided',
-        },
-      };
+      return badRequest(new Error('No question provided'));
     }
 
     const { question } = requestBody;
@@ -26,30 +22,13 @@ export async function chat(request: HttpRequest, context: InvocationContext): Pr
 
     const promptResponse = await embeddings.embedQuery(prompt);
 
-    if (promptResponse) {
-      return {
-        status: 200,
-        jsonBody: {
-          promptResponse,
-        },
-      };
-    }
-
-    return {
-      status: 503,
-      jsonBody: {
-        error: 'Service temporarily unavailable. Please try again later.',
-      },
-    };
+    return promptResponse
+      ? ok({ promptResponse })
+      : serviceUnavailable(new Error('Service temporarily unavailable. Please try again later.'));
   } catch (error: unknown) {
     const error_ = error as Error;
     context.error(`Error when processing chat request: ${error_.message}`);
 
-    return {
-      status: 503,
-      jsonBody: {
-        error: 'Service temporarily unavailable. Please try again later.',
-      },
-    };
+    return serviceUnavailable(new Error('Service temporarily unavailable. Please try again later.'));
   }
 }
