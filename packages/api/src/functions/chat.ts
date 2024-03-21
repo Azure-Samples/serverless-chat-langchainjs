dd2b9d865b7436134bcba1045c51e21d83b03f5a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-03-20 22:34:12 -0300

    chore: add chat functionality and file upload endpoints

--- packages/api/src/functions/chat.ts
+++ packages/api/src/functions/chat.ts
@@ -1,7 +1,12 @@
 import { HttpRequest, InvocationContext, HttpResponseInit } from '@azure/functions';
 import { AzureOpenAIEmbeddings, AzureChatOpenAI } from '@langchain/azure-openai';
-import 'dotenv/config';
 import { badRequest, serviceUnavailable, ok } from '../utils';
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { AzureCosmosDBVectorStore } from "@langchain/community/vectorstores/azure_cosmosdb";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+
+import 'dotenv/config';
 
 export async function chat(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
@@ -20,11 +25,43 @@ export async function chat(request: HttpRequest, context: InvocationContext): Pr
     const prompt = `Question: ${question}`;
     context.log(`Sending prompt to the model: ${prompt}`);
 
-    const promptResponse = await embeddings.embedQuery(prompt);
+    const model = new AzureChatOpenAI();
 
-    return promptResponse
-      ? ok({ promptResponse })
-      : serviceUnavailable(new Error('Service temporarily unavailable. Please try again later.'));
+    const questionAnsweringPrompt = ChatPromptTemplate.fromMessages([
+      [
+        "system",
+        "Answer the user's questions based on the below context:\n\n{context}",
+      ],
+      ["human", "{input}"],
+    ]);
+
+    const combineDocsChain = await createStuffDocumentsChain({
+      llm: model,
+      prompt: questionAnsweringPrompt,
+    });
+
+    const store = new AzureCosmosDBVectorStore(
+      embeddings, {
+      databaseName: "langchaindatabase",
+      collectionName: "pdfs"
+    }
+    );
+
+    const chain = await createRetrievalChain({
+      retriever: store.asRetriever(),
+      combineDocsChain,
+    });
+
+    const response = await chain.invoke({
+      input: question
+    });
+
+    return {
+      status: 200,
+      jsonBody: {
+        response,
+      },
+    }
   } catch (error: unknown) {
     const error_ = error as Error;
     context.error(`Error when processing chat request: ${error_.message}`);
