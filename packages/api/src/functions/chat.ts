8b9e044d7c94aa0fd72d47e84099f3a38ebc20c8
sinedied <noda@free.fr>
2024-03-25 15:10:50 +0100

    chore: fix lint issues

--- packages/api/src/functions/chat.ts
+++ packages/api/src/functions/chat.ts
@@ -1,12 +1,11 @@
 import { HttpRequest, InvocationContext, HttpResponseInit } from '@azure/functions';
 import { AzureOpenAIEmbeddings, AzureChatOpenAI } from '@langchain/azure-openai';
-import { badRequest, serviceUnavailable, ok } from '../utils';
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
-import { AzureCosmosDBVectorStore } from "@langchain/community/vectorstores/azure_cosmosdb";
-import { createRetrievalChain } from "langchain/chains/retrieval";
-
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { createStuffDocumentsChain } from 'langchain/chains/combine_documents';
+import { AzureCosmosDBVectorStore } from '@langchain/community/vectorstores/azure_cosmosdb';
+import { createRetrievalChain } from 'langchain/chains/retrieval';
 import 'dotenv/config';
+import { badRequest, serviceUnavailable, ok } from '../utils';
 
 export async function chat(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
@@ -28,18 +27,15 @@ export async function chat(request: HttpRequest, context: InvocationContext): Pr
     const model = new AzureChatOpenAI();
 
     const questionAnsweringPrompt = ChatPromptTemplate.fromMessages([
-      [
-        "system",
-        "Answer the user's questions based on the below context:\n\n{context}",
-      ],
-      ["human", "{input}"],
+      ['system', "Answer the user's questions based on the below context:\n\n{context}"],
+      ['human', '{input}'],
     ]);
 
     const combineDocsChain = await createStuffDocumentsChain({
       llm: model,
       prompt: questionAnsweringPrompt,
     });
-    
+
     const store = new AzureCosmosDBVectorStore(embeddings, {});
 
     const chain = await createRetrievalChain({
@@ -48,7 +44,7 @@ export async function chat(request: HttpRequest, context: InvocationContext): Pr
     });
 
     const response = await chain.invoke({
-      input: question
+      input: question,
     });
 
     return response
--- packages/api/src/functions/upload.ts
+++ packages/api/src/functions/upload.ts
@@ -1,14 +1,13 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
 import { AzureOpenAIEmbeddings } from '@langchain/azure-openai';
-import { badRequest, serviceUnavailable, ok } from '../utils';
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import {
   AzureCosmosDBVectorStore,
   AzureCosmosDBSimilarityType,
-} from "@langchain/community/vectorstores/azure_cosmosdb";
-
+} from '@langchain/community/vectorstores/azure_cosmosdb';
 import 'dotenv/config';
+import { badRequest, serviceUnavailable, ok } from '../utils';
 
 export async function upload(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   try {
@@ -33,16 +32,12 @@ export async function upload(request: HttpRequest, context: InvocationContext): 
 
     const documents = await splitter.splitDocuments(rawDocument);
 
-    const store = await AzureCosmosDBVectorStore.fromDocuments(
-      documents,
-      new AzureOpenAIEmbeddings(),
-      {},
-    );
+    const store = await AzureCosmosDBVectorStore.fromDocuments(documents, new AzureOpenAIEmbeddings(), {});
 
-    const numLists = 100;
+    const numberLists = 100;
     const dimensions = 1536;
     const similarity = AzureCosmosDBSimilarityType.COS;
-    await store.createIndex(numLists, dimensions, similarity);
+    await store.createIndex(numberLists, dimensions, similarity);
 
     await store.close();
 
@@ -53,4 +48,4 @@ export async function upload(request: HttpRequest, context: InvocationContext): 
 
     return serviceUnavailable(new Error('Service temporarily unavailable. Please try again later.'));
   }
-};
+}
