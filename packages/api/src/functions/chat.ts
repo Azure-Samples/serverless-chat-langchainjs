63acf19808523e7cee2ee15953ea75a4e8070b9c
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-03-27 15:25:25 -0300

    chore: implement stream logic in the `chat` API

--- packages/api/src/functions/chat.ts
+++ packages/api/src/functions/chat.ts
@@ -1,3 +1,6 @@
+import { Readable } from 'node:stream';
+import { IterableReadableStream } from '@langchain/core/dist/utils/stream';
+import { Document } from '@langchain/core/documents';
 import { HttpRequest, InvocationContext, HttpResponseInit } from '@azure/functions';
 import { AzureOpenAIEmbeddings, AzureChatOpenAI } from '@langchain/azure-openai';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
@@ -5,7 +8,7 @@ import { createStuffDocumentsChain } from 'langchain/chains/combine_documents';
 import { AzureCosmosDBVectorStore } from '@langchain/community/vectorstores/azure_cosmosdb';
 import { createRetrievalChain } from 'langchain/chains/retrieval';
 import 'dotenv/config';
-import { badRequest, serviceUnavailable, ok } from '../utils';
+import { badRequest, serviceUnavailable, okStreamResponse } from '../utils';
 
 export async function chat(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log(`Http function processed request for url "${request.url}"`);
@@ -43,17 +46,43 @@ export async function chat(request: HttpRequest, context: InvocationContext): Pr
       combineDocsChain,
     });
 
-    const response = await chain.invoke({
+    const response = await chain.stream({
       input: question,
     });
 
-    return response
-      ? ok({ response })
-      : serviceUnavailable(new Error('Service temporarily unavailable. Please try again later.'));
+    return {
+      ...okStreamResponse(),
+      body: createStream(response),
+    };
   } catch (error: unknown) {
     const error_ = error as Error;
     context.error(`Error when processing chat request: ${error_.message}`);
 
     return serviceUnavailable(new Error('Service temporarily unavailable. Please try again later.'));
   }
+}
+
+function createStream(
+  chunks: IterableReadableStream<
+    {
+      context: Document[];
+      answer: string;
+    } & Record<string, unknown>
+  >,
+) {
+  const buffer = new Readable({
+    read() {},
+  });
+
+  const stream = async () => {
+    for await (const chunk of chunks) {
+      buffer.push(chunk.answer);
+    }
+
+    buffer.push(null);
+  };
+
+  stream();
+
+  return buffer;
 }
