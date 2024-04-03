3ca9c66c676bcccd5decd78556ea3a0196224b1b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-04-02 23:26:08 -0300

    fix: remove `index.ts` in the root of the project to let more simpler

--- packages/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { app } from '@azure/functions';
-import { chat } from './functions/chat';
-
-app.setup({ enableHttpStream: true });
-app.post('chat', {
-  route: 'chat',
-  authLevel: 'anonymous',
-  handler: chat,
-});
