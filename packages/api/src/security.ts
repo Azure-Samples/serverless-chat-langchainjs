af849b7327bc13e2e8db6e8975881acc7fbec065
sinedied <noda@free.fr>
2024-04-30 15:20:10 +0200

    chore: fix typo

--- packages/api/src/security.ts
+++ packages/api/src/security.ts
@@ -4,7 +4,7 @@ let credentials: DefaultAzureCredential | undefined;
 
 export function getCredentials(): DefaultAzureCredential {
   // Use the current user identity to authenticate.
-  // No secrets needed, it uses 'az loginz or 'azd auth login' locally,
+  // No secrets needed, it uses `az login` or `azd auth login` locally,
   // and managed identity when deployed on Azure.
   credentials ||= new DefaultAzureCredential();
   return credentials;
